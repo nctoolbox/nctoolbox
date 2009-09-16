7527cca3de0433772caa0a02825f4524e4117119
Brian Schlining <brian@mbari.org>
2009-09-16 16:12:30 -0700

    Fixed an error in the packaging script when I renamed the project from ncdataset to nctoolbox

--- package.sh
+++ package.sh
@@ -7,11 +7,11 @@
 
 TARGET=target/nctoolbox
 mkdir -p $TARGET
-cp -R data $TARGET/cdm
+cp -R cdm $TARGET/cdm
 cp -R demos $TARGET/demos
 cp -R java $TARGET/java
 cp README $TARGET
-cp setup_nctoolboxt.m $TARGET
+cp setup_nctoolbox.m $TARGET
 rm -rf $(find $TARGET -name .svn)
 ditto -c -k --keepParent -rsrc $TARGET nctoolbox-$(date "+%Y%m%d").zip
 rm -rf target
