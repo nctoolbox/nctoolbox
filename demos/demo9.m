740270cd4fd8a24de9347a3e6311880995651f80
Brian Schlining <brian@mbari.org>
2010-01-25 16:54:52 -0800

    Adding demo9

---
+++ demos/demo9.m
@@ -0,0 +1,40 @@
+% DEMO9 - Like demo8 but uses 'struct' sytax to access a variable
+% and it's coordinate axis data
+
+echo('on')
+% Demonstration of subsetting a CF convention dataset
+
+url='http://blackburn.whoi.edu:8081/thredds/dodsC/coawst/fmrc/coawst_2_best.ncd';
+ds = cfdataset(url);
+
+% Grab the variable of interest. No data is being read yet.
+sz = ds.size('temp');
+
+% Grab a subset of the data. Data is now being pulled across the network
+t = ds.struct('temp', [sz(1) sz(2) 1 1], [sz(1) sz(2) sz(3) sz(4)]);
+
+% Make a pretty plot
+surf(t.lon_rho, t.lat_rho, double(squeeze(t.temp)))
+shading('interp');
+view(2)
+axis('equal')
+
+xatt = ds.attributes('lon_rho');
+xname = value4key(xatt, 'long_name');
+xunits = value4key(xatt, 'units');
+xlabel([xname ' [' xunits ']']);
+
+yatt = ds.attributes('lat_rho');
+yname = value4key(yatt, 'long_name');
+yunits = value4key(yatt, 'units'); 
+ylabel([yname ' [' yunits ']']);
+
+zatt = ds.attributes('temp');
+zname = value4key(zatt, 'long_name');
+zunits = value4key(zatt, 'units'); 
+ztime = ds.time('ocean_time', t.ocean_time);
+title([zname ' [' zunits '] on ' datestr(ztime)]);
+
+colorbar
+
+echo('off')
