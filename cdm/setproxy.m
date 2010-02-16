2132b174c556d1063ba4d88654d1f71f78525a8c
Brian Schlining <brian@mbari.org>
2010-02-16 14:45:40 -0800

    Added 1st cut a proxy support

---
+++ cdm/setproxy.m
@@ -0,0 +1,17 @@
+function setproxy(proxyname, port)
+% SETPROXY - Set the proxy to use when making all remote calls with
+% nctoolbox
+%
+% Use as: setproxy(proxyname, port)
+%
+% Inputs:
+%  proxyname = the proxy name that all connections will be routed to
+%  port      = The port on the proxy to connect through
+%  
+
+% Brian Schlining
+% 2010-02-16
+
+httpClient = org.apache.commons.httpclient.HttpClient()
+httpClient.getHostConfiguration().setProxy(proxyname, port)
+ucar.nc2.dataset.NetcdfDataset.setHttpClient(httpClient)
