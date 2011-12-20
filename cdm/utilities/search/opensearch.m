641a89fdf3f40f3c8b8ccfdee22c4f455771ce1f
Rich Signell <rsignell@usgs.gov>
2011-12-20 09:19:48 -0500

    modified opensearch to allow more Matlab-like time and bbox inputs

--- cdm/utilities/search/opensearch.m
+++ cdm/utilities/search/opensearch.m
@@ -1,13 +1,15 @@
-function [ links, params, requesturl] = opensearch( q )
-%OPENSEARCH Queries GI-cat using the opensearch interface
-% returns a list of available links from the matching resources
+function [ links, params, requesturl, box] = opensearch( q )
+%OPENSEARCH Queries GI-cat using the OpenSearch interface
+% Returns a list of available links from the matching resources
+%
 % Usage:  [ links, params, requesturl ] = opensearch( q )
 %
 %   q.endpoint = URL to opensearch server
 %   q.string_text = text string to match 
-%   q.bbox= 'xmin,ymin,xmax,ymax'
-%   q.time_start = start time string
-%   q.time_end  = ending time string
+%   q.bbox= bounding box, vector [lon_min lon_max lat_min lat_max]
+%       or string 'lon_min,lat_min,lon_max,lat_max' 
+%   q.time_start = start time, datenum/datestr or ISO string
+%   q.time_end  = ending time, datenum/datestr or ISO string
 % 
 % For full options and more explanation, see the GI-CAT OpenSearch doc
 %    http://essi-lab.eu/cgi-bin/twiki/view/GIcat/OpenSearchGuide
@@ -15,28 +17,26 @@ function [ links, params, requesturl] = opensearch( q )
 % Example 1:
 % q.endpoint='http://geoport.whoi.edu/gi-cat/services/opensearch';
 % q.string_text='sea_water_temperature';
-% q.bbox='-74,39,-66,45';
-% q.time_start='1999-01-20T04:33:45Z';
-% q.time_end='1999-08-01T00:00:00Z';
+% q.bbox=[-74.0 -66.0 39.0 45.];
+% q.time_start='1999-01-20 04:33:45';
+% q.time_end='1999-08-01 00:00:00';
 % [links,params]=opensearch(q);   % make the query
 % dap=links2dap(links); % find only the OPeNDAP links
 %
 % Example 2:
 % q.endpoint='http://testbedapps.sura.org/gi-cat/services/opensearch';
 % q.string_text='sea_water_salinity';
-% q.bbox='-82,36,-73,40';
-% q.time_start='2000-06-20T04:33:45Z';
-% q.time_end='2006-08-01T00:00:00Z';
+% q.bbox=[-82 -73 36 40];
+% q.time_start=[2000 06 20 04 33 45];
+% q.time_end=[2006 08 01 00 00 00];
 % [links,params]=opensearch(q);   % make the query
 % dap=links2dap(links); % find only the OPeNDAP links
 % 
 % Example 3:
 % q.endpoint='http://geoport.whoi.edu/gi-cat/services/opensearch';
-% q.bbox='-180,0,180,90'; % northern hemisphere
-% start=now_utc-7;  %start one week ago
-% stop=now_utc;     %stop now
-% q.time_start=datestr(start,'yyyy-mm-ddTHH:MM:SSZ');% convert to ISO
-% q.time_end=datestr(stop,'yyyy-mm-ddTHH:MM:SSZ');% convert to ISO
+% q.bbox=[-180 180 -30 70];  % whole globe from -30 to 70 north
+% q.time_start=now_utc-28; % start 28 days ago
+% q.time_end=now_utc;    % stop now
 % q.string_text='relative_humidity'; %search for datasets containing 'relative_humidity'
 % [links,params]=opensearch(q);   % make the query
 % dap=links2dap(links); % find only the OPeNDAP links
@@ -44,9 +44,31 @@ function [ links, params, requesturl] = opensearch( q )
 if(nargin<1);help opensearch; return;end
 if ~isfield(q,'endpoint'); disp('must define endpoint (URL)'); return; end
 if ~isfield(q,'string_text'); q.string_text='';end
-if ~isfield(q,'bbox'); q.bbox='';end
-if ~isfield(q,'time_start'); q.time_start='';end
-if ~isfield(q,'time_end'); q.time_end='';end
+if ~isfield(q,'bbox')
+  q.bbox='';
+else
+  if length(q.bbox)==4, % matlab style bbox [lon_min lon_max lat_min lat_max]
+    q.bbox=sprintf('%g, %g, %g, %g',q.bbox([1 3 2 4]));
+  end
+end
+if ~isfield(q,'time_start')
+  q.time_start='';
+else
+  try 
+    dn=datenum(q.time_start); % handle datenum/datestr input
+    q.time_start=datestr(dn,'yyyy-mm-ddTHH:MM:SSZ');
+  catch
+  end 
+end
+if ~isfield(q,'time_end')
+  q.time_end='';
+else
+  try 
+    dn=datenum(q.time_end);
+    q.time_end=datestr(dn,'yyyy-mm-ddTHH:MM:SSZ');
+  catch
+  end
+end
 if ~isfield(q,'loc'); q.loc='';end
 if ~isfield(q,'si'); q.si='1';end
 
@@ -72,7 +94,6 @@ params = {
 'outputFormat';'application/atom+xml';
 'enableCrawling'; 'false';
 };
-
 [results] = urlread(q.endpoint,'get', params);
 % find all URL links
 [~,~,~,~,links]=regexp(results,'<gmd:URL>(.*?)</gmd:URL>','match');
