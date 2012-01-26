e179ee7edfe6c63571a700795339de971dd5d6b2
Alexander Crosby <devnull@localhost>
2012-01-26 10:22:21 -0500

    add haversine calculation to nearxy.m, so that it actually is a distance measure

--- cdm/utilities/misc/nearxy.m
+++ cdm/utilities/misc/nearxy.m
@@ -6,17 +6,24 @@ function [index,distance, twoout]=nearxy(x,y,x0,y0,dist);
 %                                           the value of dist.
 % rsignell@crusty.er.usgs.gov
 %
-distance=sqrt((x-x0).^2+(y-y0).^2);
+
+% distance=sqrt((x-x0).^2+(y-y0).^2);
+distance = zeros(size(x));
+for i = 1:length(x(:,1))
+    for j = 1:length(x(1,:))
+        distance(i,j) = haversine([y(i,j), x(i,j)], [y0, x0]); %this is actually a real distance calculation now...
+    end
+end
 if (nargin > 4),
-  index=find(distance<=dist);     %finds points closer than dist
-  [row col] = find(distance<=dist);
-  
+    index=find(distance<=dist);     %finds points closer than dist
+    [row col] = find(distance<=dist);
+    
 else
-  index=find(distance==min(min(distance)));  % finds closest point
-   [row col]=find(distance==min(min(distance)));
-  index=index(1);
-  
+    index=find(distance==min(min(distance)));  % finds closest point
+    [row col]=find(distance==min(min(distance)));
+    index=index(1);
+    
 end
 distance=distance(index);
- twoout = [row col];
+twoout = [row col];
 end
