0a90c83b60bb4ae50c68261ccac02cd2cd3f372a
Alexander Crosby <devnull@localhost>
2012-02-02 15:03:18 -0500

    complex_ongrid.m, geodemo_6.m, removed the extra classes to start over

---
+++ cdm/utilities/interp/complex_ongrid.m
@@ -0,0 +1,173 @@
+classdef complex_ongrid < handle
+    
+    properties (SetAccess = private)
+        uvar
+        vvar
+        rhovar
+        anglevar
+    end % end properties
+    
+    
+    methods
+        function obj = complex_ongrid(rhovar, ncgeovariable_u, ...
+                ncgeovariable_v, ncgeovariable_angle)
+            obj.uvar = ncgeovariable_u;
+            obj.vvar = ncgeovariable_v;
+            if nargin < 4
+                obj.anglevar = -1;
+            else
+                obj.anglevar = ncgeovariable_angle;
+            end
+            obj.rhovar = rhovar;
+        end % end constructor
+        
+        function mag = magnitude(obj, first, last)
+            vec = obj.vectors(first, last);
+            mag = abs(vec);
+        end % end mag
+        
+        function vec = vectors(obj, first, last)
+            % get lon,lat size from hvar
+            if nargin < 2
+                hsize = obj.rhovar.size;
+                horder = obj.rhovar.getaxesorder;
+                if horder.lat == horder.lon
+                    jj = 1:hsize(horder.lat);
+                    ii = 1:hsize(horder.lat  + 1);
+                else
+                    %         ii = hsize(order.lat);
+                    %         jj = hsize(order.lon);
+                    error('order.lat != order.lon');
+                end
+            else
+                jj = first(end-1):last(end-1);
+                ii = first(end):last(end);
+            end
+            ujj = (jj(1) + 1):(jj(end) - 1);
+            uii = ii(1):ii(end) - 1;
+            vjj = jj(1):jj(end) - 1;
+            vii = (ii(1) + 1):(ii(end) - 1);
+            
+            u = double(squeeze(obj.uvar.data(first(1), first(2), ujj, uii))); % get u
+            v = double(squeeze(obj.vvar.data(first(1), first(2), vjj, vii))); % get v
+            g = obj.rhovar.grid_interop(jj, ii); % get lon,lat from rho-point variable (like 'h' in ROMS)
+            
+            U = ones(size(g.lon)) * nan; % template for U at rho points
+            U(2:end-1, 2:end-1) = complex(av2(u.').', av2(v)); %average u,v to rho
+            
+            if obj.anglevar ~= -1
+                ang = obj.anglevar.data(jj, ii); % get angle
+                U = U .* exp(sqrt(-1) * ang); % rotate
+            end
+            
+            vec = U; % output
+            
+        end % end vec
+        
+        function g = grid(obj, first, last)
+            if nargin < 2
+                hsize = obj.rhovar.size;
+                horder = obj.rhovar.getaxesorder;
+                if horder.lat == horder.lon
+                    jj = 1:hsize(horder.lat);
+                    ii = 1:hsize(horder.lat  + 1);
+                else
+                    %         ii = hsize(order.lat);
+                    %         jj = hsize(order.lon);
+                    error('order.lat != order.lon');
+                end
+            else
+                jj = first(end-1):last(end-1);
+                ii = first(end):last(end);
+            end
+            g = obj.rhovar.grid_interop(jj, ii); % get lon,lat from rho-point variable (like 'h' in ROMS)
+            
+            % output
+            tim = obj.uvar.timewindowij( double(first(1)) );
+            g.time = tim.time;
+            g.klevel = first(2);
+            g.itime = first(2);
+            
+        end % end grid
+        
+        function e = end(obj, k, n)
+            usize = size(obj.uvar);
+            hsize = size(obj.rhovar);
+            n = [usize(1:2) hsize(end-1:end)];
+            e = n(k);
+        end % Added to deal with end indexing functionality,
+        % otherwise the indexing arugment is ignored.
+        
+        function sref = subsref(obj,s)
+            switch s(1).type
+                % Use the built-in subsref for dot notation
+                case '.'
+                    usize = size(obj.uvar);
+                    hsize = size(obj.rhovar);
+                    nums = [usize(1:2) hsize(end-1:end)];
+                    switch s(1).subs
+                        case 'grid'
+                            
+                            if ~isempty(nums)
+                                switch length(s)
+                                    case 1
+                                        sref = obj;
+                                    case 2
+                                        [first last stride] = indexing(s(2).subs, double(nums));
+                                        sref = obj.grid(first, last);
+                                end
+                                
+                            else
+                                sref = obj.data;
+                                warning('NCTOOLBOX:ncvariable:subsref', ...
+                                    ['Variable "' obj.name '" has no netcdf dimension associated with it. Errors may result from non CF compliant files.'])
+                            end
+                        case 'vectors'
+                            %                             nums = size(obj.rhovar);
+                            if ~isempty(nums)
+                                switch length(s)
+                                    case 1
+                                        sref = obj;
+                                    case 2
+                                        [first last stride] = indexing(s(2).subs, double(nums));
+                                        sref = obj.vectors(first, last);
+                                end
+                                
+                            else
+                                warning('NCTOOLBOX:ncvariable:subsref', ...
+                                    ['Variable "' name '" has no netcdf dimension associated with it. Errors may result from non CF compliant files.'])
+                            end
+                        case 'magnitude'
+                            %                             nums = size(obj.rhovar);
+                            if ~isempty(nums)
+                                switch length(s)
+                                    case 1
+                                        sref = obj;
+                                    case 2
+                                        [first last stride] = indexing(s(2).subs, double(nums));
+                                        sref = obj.magnitude(first, last);
+                                end
+                                
+                            else
+                                warning('NCTOOLBOX:ncvariable:subsref', ...
+                                    ['Variable "' name '" has no netcdf dimension associated with it. Errors may result from non CF compliant files.'])
+                            end
+                        otherwise
+                            sref = builtin('subsref',obj,s);
+                    end
+                case '()'
+                    if length(s)<2
+                        % Note that obj.Data is passed to subsref
+                        sref = builtin('subsref',obj.data,s);
+                    else
+                        sref = builtin('subsref',obj,s);
+                    end
+                    % No support for indexing using '{}'
+                case '{}'
+                    error('NCTOOLBOX:ncvariable:subsref', ...
+                        'Not a supported subscripted reference, "{}" are not permitted to call variable object methods');
+            end
+        end
+    end % end methods
+    
+end % end classdef
--- demos/geodemo_6.m
+++ demos/geodemo_6.m
@@ -10,29 +10,35 @@ aname = 'angle';
 
 nc = ncgeodataset(url);
 
+uvar = nc.geovariable(uname);
+vvar = nc.geovariable(vname);
+hvar = nc.geovariable(hname);
+avar = nc.geovariable(aname);
+
 itime = 3; % 3rd time step
 klev = -1; % last (top) layer
 
 %Whole domain:
-[ U, g ] = cgrid_uv2rho(nc, uname, vname, hname, aname, itime, klev);
+% [ U, g ] = cgrid_uv2rho(nc, uname, vname, hname, aname, itime, klev);
+Uobj = complex_ongrid(hvar, uvar, vvar, avar);
+g = Uobj.grid(itime, klev, :, :);
 
 % Initialize Plot / Plot Vectors
 figure;
-pcolorjw(g.lon, g.lat, abs(U));
+pcolorjw(g.lon, g.lat, Uobj.magnitude(itime, klev, :, :));
 colorbar;
-dasp(44);
-arrows(g.lon(1:2:end,1:2:end), g.lat(1:2:end,1:2:end),...
-    U(1:2:end,1:2:end), 0.08, 'black');
+arrows(g.lon(1:end,1:end), g.lat(1:end,1:end),...
+    Uobj.vectors(itime, klev, 1:end,1:end), 0.08, 'black');
 title(datestr(g.time));
 dasp(44);
 
 %Subset to jj,ii range: New Plot / New Vectors
 figure;
-[ U, g ] = cgrid_uv2rho(nc, uname, vname, hname, aname, itime, klev, 1:58, 1:70);
-pcolorjw(g.lon, g.lat, abs(U));
+% [ U, g ] = cgrid_uv2rho(nc, uname, vname, hname, aname, itime, klev, 1:58, 1:70);
+pcolorjw(g.lon(1:58, 1:70), g.lat(1:58, 1:70), ...
+    Uobj.magnitude(itime, klev, 1:58, 1:70));
 colorbar; 
-dasp(44);
-arrows(g.lon(1:2:end,1:2:end), g.lat(1:2:end,1:2:end),...
-    U(1:2:end,1:2:end), 0.08, 'black');
+arrows(g.lon(1:58,1:70), g.lat(1:58,1:70),...
+    Uobj.vectors(itime, klev, 1:58,1:70), 0.08, 'black');
 title(datestr(g.time));
 dasp(44);
